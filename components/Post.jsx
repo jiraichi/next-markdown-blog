286d05536043763474e8ca124d1fd51fd876f14f
jiraichi <shanvaliyev@hotmail.com>
2021-12-01 02:43:43 +0800

    feat: home page setup

---
+++ components/Post.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+const Post = ({ post }) => {
+  return (
+    <div className="card">
+      <img
+        src={post.frontmatter.cover_image}
+        alt={post.frontmatter.title + ' cover image'}
+      />
+
+      <div className="post-date">Posted on {post.frontmatter.date}</div>
+
+      <h3>{post.frontmatter.title}</h3>
+      <p>{post.frontmatter.excerpt}</p>
+      <Link href={`/blog/${post.slug}`}>
+        <a className="btn">Read more</a>
+      </Link>
+    </div>
+  )
+}
+
+export { Post as default }
---
+++ pages/index.jsx
@@ -0,0 +1,54 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+import Head from 'next/head'
+
+import Post from '../components/Post'
+
+const getStaticProps = async () => {
+  // apparently nextjs is smart enough to allow us to use fs in here (normally using fs in client side code will not compile)
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map(fileName => {
+    const slug = fileName.replace('.md', '')
+
+    const markDownWithMeta = fs.readFileSync(
+      path.join('posts', fileName),
+      'utf-8'
+    )
+
+    const { data: frontmatter } = matter(markDownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
+
+const Home = ({ posts }) => {
+  console.log(posts)
+  return (
+    <div>
+      <Head>
+        <title>My Blog</title>
+        <meta name="description" content="A project created for fun." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="posts">
+        {posts.map((post, index) => {
+          return <Post post={post} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export { Home as default, getStaticProps }
